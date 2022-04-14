6ad4a3bcd398919c7ac24e476dcf79bb7e64c152
Azza893 <azzae893@gmail.com>
2022-04-14 12:36:09 +0200

    Add project files.

---
+++ BookStore/Models/Repository/AuthorRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repository
+{
+    public class AuthorRepository : IBookStoreRepository<Author>
+    {
+        List<Author> authors;
+        public AuthorRepository()
+        {
+            authors = new List<Author>()
+            {
+                new Author{Id=1, FullName="Gain of my life"},
+                new Author{Id=1, FullName="azza dawoud"},
+                new Author{Id=1, FullName="Mohamed Elsayed"},
+                new Author{Id=1, FullName="Fatma dawoud"}
+            };
+        }
+        public void Add(Author entity)
+        {
+            authors.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            authors.Remove(author);
+        }
+
+        public Author Find(int id)
+        {
+            var author = authors.SingleOrDefault(x => x.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(int id, Author entity)
+        {
+            var author = Find(id);
+            entity.FullName = author.FullName;
+        }
+    }
+}
---
+++ BookStore/Models/Repository/BookRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repository
+{
+    public class BookRepository:IBookStoreRepository<Book>
+    {
+        List<Book> books;
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book
+                {
+                    Id=1,Title="C#",Description="NO"
+                },
+                new Book
+                {
+                    Id=2,Title="C++",Description="NO"
+                },
+                new Book
+                {
+                    Id=3,Title="C",Description="NO"
+                }
+            };
+
+        }
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            books.Remove(book);
+
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(x => x.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id, Book entity)
+        {
+            var book = Find(id);
+            book.Title = entity.Title;
+            book.Description = entity.Description;
+        }
+    }
+}
